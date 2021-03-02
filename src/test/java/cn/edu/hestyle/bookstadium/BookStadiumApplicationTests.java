28003729433e41c66fa07c41d0d6b03c70faa0c4
hestyle <hestyle@qq.com>
2021-03-02 20:30:58 +0800

    first commit

---
+++ src/test/java/cn/edu/hestyle/bookstadium/BookStadiumApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.hestyle.bookstadium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStadiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
