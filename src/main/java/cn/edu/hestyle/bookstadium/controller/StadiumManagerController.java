62c75be6317ac35f9c22ba5b2b2031ff96ea8290
hestyle <hestyle@qq.com>
2021-03-05 17:31:57 +0800

    feat：完成stadium manager修改账号图片

--- src/main/java/cn/edu/hestyle/bookstadium/controller/StadiumManagerController.java
+++ src/main/java/cn/edu/hestyle/bookstadium/controller/StadiumManagerController.java
@@ -1,5 +1,6 @@
 package cn.edu.hestyle.bookstadium.controller;
 
+import cn.edu.hestyle.bookstadium.controller.exception.FileUploadFailedException;
 import cn.edu.hestyle.bookstadium.controller.exception.NotLoginException;
 import cn.edu.hestyle.bookstadium.controller.exception.RequestParamException;
 import cn.edu.hestyle.bookstadium.entity.StadiumManager;
@@ -10,14 +11,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 /**
  * @author hestyle
@@ -27,6 +32,19 @@ import java.util.Map;
 @RestController
 @RequestMapping("/stadiumManager")
 public class StadiumManagerController extends BaseController {
+    /**允许上传文件的名称*/
+    public static final String UPLOAD_DIR_NAME = "upload/image/stadiumManager/avatar";
+
+    /**上传文件的大小*/
+    public static final long FILE_MAX_SIZE = 5 * 1024 * 1024;
+
+    /**允许上传的文件类型*/
+    public static final List<String> FILE_CONTENT_TYPES = new ArrayList<>();
+
+    static {
+        FILE_CONTENT_TYPES.add("image/jpeg");
+        FILE_CONTENT_TYPES.add("image/png");
+    }
 
     private static final Logger logger = LoggerFactory.getLogger(StadiumManagerController.class);
 
@@ -103,5 +121,60 @@ public class StadiumManagerController extends BaseController {
         // 执行业务端的业务
         stadiumManagerService.changePassword(username, beforePassword, newPassword);
         return new ResponseResult<>(SUCCESS, "密码修改成功！");
+    }
+
+    @PostMapping("/uploadAvatar.do")
+    public ResponseResult<String> handleUploadAvatar(@RequestParam("file") MultipartFile file, HttpSession session) {
+        // 判断是否已经登录过
+        String username = (String) session.getAttribute("stadiumManagerUsername");
+        if (null == username) {
+            throw new NotLoginException("请求失败，请先进行登录！");
+        }
+        // 检查文件类型、大小
+        if (file.isEmpty()) {
+            logger.warn("StadiumManager  = " + username + "，文件上传失败，文件为空！");
+            throw new FileUploadFailedException("上传失败，没有选择上传的文件，或选中的文件为空!");
+        }
+        if (file.getSize() > FILE_MAX_SIZE) {
+            logger.warn("StadiumManager  = " + username + "文件上传失败，上传的文件超过5MB！");
+            throw new FileUploadFailedException("上传失败，上传的文件超过5MB！");
+        }
+        if (!FILE_CONTENT_TYPES.contains(file.getContentType())) {
+            logger.warn("StadiumManager username = " + username + "上传失败，文件类型非法，contentType = " + file.getContentType() + "只允许上传图片！");
+            throw new FileUploadFailedException("上传失败，文件类型非法，只允许上传图片文件！");
+        }
+        // 检查保存上传文件的文件夹存在(一个是在resource一个是在target下)
+        String pathNameTemp = null;
+        try {
+            pathNameTemp = ResourceUtils.getURL("classpath:").getPath() + "static/" + UPLOAD_DIR_NAME;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String pathNameTruth = pathNameTemp.replace("target", "src").replace("classes", "main/resources");
+        File parentTemp = new File(pathNameTemp);
+        File parentTruth = new File(pathNameTruth);
+        if (!parentTemp.exists()) {
+            parentTemp.mkdirs();
+        }
+        if (!parentTruth.exists()) {
+            parentTruth.mkdirs();
+        }
+        // 重新命令、保存上传文件
+        String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf('.'));
+        String saveFileName = System.currentTimeMillis() + "" + (new Random().nextInt(90000000) + 10000000) + suffix;
+        File destTemp = new File(parentTemp, saveFileName);
+        File destTruth = new File(parentTruth, saveFileName);
+        try {
+            // 写到target，再copy至resource
+            file.transferTo(destTemp);
+            FileCopyUtils.copy(destTemp, destTruth);
+            System.err.println("文件上传完成！");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseResult<>(FAILURE, "上传失败，发生未知异常！");
+        }
+        String imagePath = "/book_stadium_online/" + UPLOAD_DIR_NAME + "/" + saveFileName;
+        logger.warn("StadiumManager username = " + username + "上传文件 url = " + imagePath + "成功！");
+        return new ResponseResult<String>(SUCCESS, "上传成功", imagePath);
     }
 }
